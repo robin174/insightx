2b12a370adf4dce89662870479cde3bca8c7d8ee
Robin Green <robin@designingreen.co.uk>
2020-10-16 15:15:58 +0100

    update 404 page

--- 404.php
+++ 404.php
@@ -8,31 +8,25 @@
 get_header(); ?>
 			
 	<div class="content">
+		<main class="main">
 
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-	
-			<main class="main small-12 medium-8 large-8 cell" role="main">
+			<div class="grid-container grid-x grid-padding-x align-center">
+                <div class="small-12 medium-10 large-10 cell">
 
-				<article class="content-not-found">
+					<article class="content-not-found">
+						<header class="article-header">
+							<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
+						</header> <!-- end article header -->
 				
-					<header class="article-header">
-						<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
-					</header> <!-- end article header -->
-			
-					<section class="entry-content">
-						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
-					</section> <!-- end article section -->
-
-					<section class="search">
-					    <p><?php get_search_form(); ?></p>
-					</section> <!-- end search section -->
-			
-				</article> <!-- end article -->
+						<section class="entry-content">
+							<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
+						</section> <!-- end article section -->
+					</article> <!-- end article -->
 	
-			</main> <!-- end #main -->
+				</div>
+            </div>
 
-		</div> <!-- end #inner-content -->
-
+		</main><!-- end #main -->
 	</div> <!-- end #content -->
 
 <?php get_footer(); ?>

5c554d56f10b2bdfeee649f9e56e9c15ac9c41e7
Robin Green <robin@designingreen.co.uk>
2020-10-16 09:07:47 +0100

    edits to footer

--- footer.php
+++ footer.php
@@ -15,13 +15,21 @@
 						<section class="foot--primary">
 							<div class="grid-container">
 								<div class="grid-x grid-padding-x">
-									<div class="small-12 medium-6 large-6 cell">
+									<div class="small-12 large-12 cell">
 										<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/insightx-logo-white-n.svg" width="100px" title="Logo - Insight.X"></a>
+									</div>
+									<div class="small-12 medium-6 large-6 cell">
 										<p class="footer--sm">Company number: 12674763<br>
 										71-75 Shelton Street, London WC2H 9JQ</p>
-										<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> Ltd. All rights reserved.</p>
 									</div>
-									<div class="small-12 medium-6 large-6 cell text-right">
+									<div class="small-12 medium-6 large-6 cell medium-text-right">
+										<div class="foot--contact">
+											<p class="footer--sm">Email InsightX on <a href="mailto:info@insightxintel.com" title="Email InsightX">info@insightxintel.com</a><br>
+											Find InsightX on <a href="https://www.linkedin.com/company/insightx/" target="_blank" title="InsightX on LinkedIn">LinkedIn</a></p>
+										</div>
+									</div>
+									<div class="small-12 large-12 cell">
+										<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> Ltd. All rights reserved.</p>
 									</div>
 								</div>
 							</div>

fc5bbb8f18bf6ff126a07c306f6bb53a28d25445
Robin Green <robin@designingreen.co.uk>
2020-10-16 14:59:55 +0100

    more minor edits to animation

--- page-animation.php
+++ page-animation.php
@@ -5,11 +5,11 @@ Template Name: Page - Animation
 get_header(); ?>
 
 	<div class="content">
-
-        <div class="animation" style="width:1400px;margin:0 auto;">
-            <?php get_template_part('templates/introduction'); ?>
-        </div>
-
+		<main class="main">
+	        <div class="animation">
+	            <?php get_template_part('templates/introduction'); ?>
+	        </div>
+		</main>
 	</div> <!-- end #content -->
 
 <?php get_footer(); ?>

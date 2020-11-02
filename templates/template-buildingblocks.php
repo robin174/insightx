6743c3d2c6dea10dc019fb60add53b138e3938d1
Robin Green <robin@designingreen.co.uk>
2020-11-02 17:41:48 +0000

    Small boilerplate tweaks

--- templates/template-buildingblocks.php
+++ templates/template-buildingblocks.php
@@ -17,36 +17,24 @@
     <?php endif; ?>
     
     <?php if(get_row_layout() == 'block_image'): // layout: Image / Caption - Full Width ?>
-        <div class="grid-container grid-x">
-            <div class="small-12 medium-12 large-12 cell">
-                <figure >
-                    <img src="<?php the_sub_field('unit_image'); ?>" alt="" />
-                    <?php if( get_sub_field('unit_image_credit') ): ?>
-                        <figcaption class="caption"><?php the_sub_field('unit_image_credit'); ?></figcaption>
-                    <?php endif; ?>
-                </figure>
-            </div>
-        </div>
+        <figure class="image-post">
+            <img src="<?php the_sub_field('unit_image'); ?>" alt="" />
+            <?php if( get_sub_field('unit_image_credit') ): ?>
+                <figcaption class="caption"><?php the_sub_field('unit_image_credit'); ?></figcaption>
+            <?php endif; ?>
+        </figure>
     <?php endif; ?>
 
     <?php if(get_row_layout() == 'block_video'): // layout: Video ?>
         <?php if(get_sub_field('unit_site') == "vimeo"): // If Vimeo Selected ?>
-            <div class="grid-container grid-x">
-                <div class="medium-offset-1 small-12 large-10 medium-10 cell">
-                    <div class="responsive-embed vimeo">
-                        <iframe width="640" height="360" src="//player.vimeo.com/video/<?php the_sub_field('unit_vimeo_link'); ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                    </div>
-                </div>
+            <div class="responsive-embed vimeo widescreen">
+                <iframe width="640" height="360" src="//player.vimeo.com/video/<?php the_sub_field('unit_vimeo_link_id'); ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
         <?php endif; ?>
 
         <?php if(get_sub_field('unit_site') == "youtube"): // If YouTube Selected ?>
-            <div class="grid-container grid-x">
-                <div class="medium-offset-1 small-12 large-10 medium-10 cell">
-                    <div class="responsive-embed youtube">
-                        <iframe width="560" height="315" src="//www.youtube.com/embed/<?php the_sub_field('unit_youtube_link'); ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                    </div>
-                </div>
+            <div class="responsive-embed youtube widescreen">
+                <iframe src="//www.youtube.com/embed/<?php the_sub_field('unit_youtube_link_id'); ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
         <?php endif; ?>
     <?php endif; ?>
@@ -95,13 +83,9 @@
     <?php endif; ?>
 
     <?php if(get_row_layout() == 'block_footnote'): // layout: Footnote ?>
-        <div class="grid-container grid-x">
-            <div class="small-12 medium-12 large-12 cell">
-                <section class="unit_footnote">
-                    <p class="note"><?php the_sub_field('unit_note'); ?></p>
-                </section>
-            </div>
-        </div>
+        <section class="unit_footnote">
+            <p class="note"><?php the_sub_field('unit_note'); ?></p>
+        </section>
     <?php endif; ?>
 
 <?php endwhile; ?>

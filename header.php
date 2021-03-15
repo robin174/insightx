bda8dd54252a4d5f3c85ffa2229375f44042adf5
Robin Green <robin@designingreen.co.uk>
2021-03-15 15:17:22 +0000

    Remove Font Awesome temp

--- header.php
+++ header.php
@@ -34,9 +34,6 @@
 		<!-- Google Font / Alt: FF Zwo -->
 		<link rel="stylesheet" href="https://use.typekit.net/ntc5nyr.css">
 
-		<!-- Font Awesome -->
-		<!-- <script src="https://kit.fontawesome.com/2301fd75c6.js" crossorigin="anonymous"></script> -->
-
 		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180735634-1"></script>
 		<script>
